3a70fb00af7e90163b66284f17fc14e7c6c3cbf5
Bert Maher <bertrand@fb.com>
2024-07-30 11:52:47 -0700

    Clean up more

--- cutlass_kernel.h
+++
@@ -1,7 +0,0 @@
-// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
-
-#pragma once
-
-#include <cuda_bf16.h>
-
-void wgmma_kernel(void* a, void* b, void* c, int m, int n, int k);
